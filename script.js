45e86fbcb43d60567c0219f2d4e3852468760bbc
ismail abdrma <88494800+ismailabdrma@users.noreply.github.com>
2023-05-29 17:04:54 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function afficherVosInformations() {
+
+  var nom = prompt("Veuillez saisir votre nom :");
+
+  var prenom = prompt("Veuillez saisir votre prénom :");
+
+  var filiere = prompt("Veuillez saisir votre filière :");
+
+  var groupe = prompt("Veuillez saisir votre groupe :");
+
+  alert("Nom : " + nom + "\nPrénom : " + prenom + "\nFilière : " + filiere + "\nGroupe : " + groupe);
+
+}
+
